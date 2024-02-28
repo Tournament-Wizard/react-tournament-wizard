118304053caa8ee307aea57758c87a5de1eecdc2
Carlos <carlosterrero2@gmail.com>
2024-02-28 17:51:18 +0100

    Add TournamentDetailsEdit view and fetch

--- src/components/structure/Navigations.jsx
+++ src/components/structure/Navigations.jsx
@@ -3,11 +3,13 @@ import Login from "../../views/Login.jsx"
 import Dashboard from "../../views/Dashboard.jsx"
 import CreateTournament from "../../views/CreateTournament.jsx"
 import TournamentDetails from "../../views/TournamentDetails.jsx"
+import TournamentDetailsEdit from "../../views/TournamentDetailsEdit.jsx"
 
 export const nav = [
-     { path:     "/",         name: "Home",        element: <Home />,       isMenu: false,     isPrivate: false  },
-     { path:     "/login",    name: "Login",       element: <Login />,      isMenu: false,    isPrivate: false  },
-     { path:     "/dashboard",  name: "Tournaments",     element: <Dashboard />,    isMenu: true,     isPrivate: true  },
-     { path:     "/dashboard/new",  name: "NewTournament",     element: <CreateTournament />,    isMenu: false,     isPrivate: true  },
-     { path:     "/tournaments/:id",  name: "Tournament",     element: <TournamentDetails />,    isMenu: false,     isPrivate: true  },
-]
+    { path: "/", name: "Home", element: <Home />, isMenu: false, isPrivate: false },
+    { path: "/login", name: "Login", element: <Login />, isMenu: false, isPrivate: false },
+    { path: "/dashboard", name: "Tournaments", element: <Dashboard />, isMenu: true, isPrivate: true },
+    { path: "/dashboard/new", name: "NewTournament", element: <CreateTournament />, isMenu: false, isPrivate: true },
+    { path: "/tournaments/:id", name: "Tournament", element: <TournamentDetails />, isMenu: false, isPrivate: true },
+    { path: "/tournaments/:id/edit", name: "TournamentEdit", element: <TournamentDetailsEdit />, isMenu: false, isPrivate: true }
+];
